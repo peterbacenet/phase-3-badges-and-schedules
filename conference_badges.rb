4e69c882ee70a2b842748f7d0aa5963929dbd265
Peter Bacenet <peterbacenet@icloud.com>
2021-09-21 21:03:31 -0400

    commit tree 41bb555fb257f69889d65064fd7e9c1d112ae04a

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator array
+    array.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms array
+    array.map.with_index(1) do |name, room|
+        "Hello, #{name}! You'll be assigned to room #{room}!"
+    end
+end
+
+def printer (array)
+    batch_badge_creator(array).map do |badge|
+        puts badge
+    end
+
+    assign_rooms(array).map do |room|
+        puts room
+    end
+end
